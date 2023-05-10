8b44564e594ab75a4d3cd26cf6225b3eb61db6d7
Hafizh ZR <hafizhzaghlul882@gmail.com>
2023-05-11 00:42:46 +0700

    second

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,10 +2,8 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'food-heaven-database',
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'foodheaven',
 };
 
 export default CONFIG;
